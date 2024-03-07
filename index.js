405f72beace137949cffd2cc35f1c0de4acaa2cb
Faraz Irfan <farazirfan47@gmail.com>
2024-03-08 01:27:19 +0500

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const { compareValues, applyPagination } = require('./utils/helper');
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
