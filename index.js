929e65b2f464a836e106829ff4d5d57565b27b8a
Faraz Irfan <farazirfan47@gmail.com>
2024-03-08 01:32:44 +0500

    fi

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const { compareValues, applyPagination } = require('./utils/helper');
 const app = express();
-const port = 80;
+const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
